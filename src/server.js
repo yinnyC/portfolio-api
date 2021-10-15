4ec5b80ae41aad2aac4a799785706b096e88d1da
Yin <yinnnn17@gmail.com>
2021-10-15 11:25:40 -0700

    [update] connect api

--- src/server.js
+++ src/server.js
@@ -1,54 +1,23 @@
+const cors = require('cors');
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const cookieParser = require('cookie-parser');
-
-const router = express.Router();
 
 // Import Routes
 const indexRouter = require('./routes/index');
 
 // Set App Variable
 const app = express();
-
-const corsOptions = {
-	origin: [
-		'*',
-		'http://localhost',
-		'http://localhost:3000',
-		'http://localhost:8100',
-		'https://yinnyc.github.io/portfolio/',
-	],
-};
-
-// Middleware
-app.use(cors(corsOptions));
-app.use(cookieParser());
+// Add cors
+app.use(
+	cors({
+		origin: ['https://yinnyc.github.io/portfolio/', '*'],
+	})
+);
 app.use(express.json());
-app.use(helmet());
 
 // Routes
 app.use('/', indexRouter);
 
-/// //////////////////////////////////////////////////////////////////////////////////////
-// Error page for error handling
-/// //////////////////////////////////////////////////////////////////////////////////////
-router.use((error, req, res, next) =>
-	res.status(error.statusCode || 500).json({
-		message: error.message || 'Internal Server Error',
-		data: error.data || null,
-	})
-);
-
-/// //////////////////////////////////////////////////////////////////////////////////////
-// If no explicit error and route requested not found
-/// //////////////////////////////////////////////////////////////////////////////////////
-router.use((req, res, next) =>
-	res.status(404).json({ message: 'API endpoint not found.' })
-);
-
-// Set db
 const run = async () => {
 	await app.listen(process.env.PORT);
 	console.log(`App listening on port ${process.env.PORT}!`);
